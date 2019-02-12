0f8e17dfc7bef8572d6d8ecaf487ff8ed32832e2
ayong8 <yong8@me.com>
2019-02-11 22:04:21 -0500

    save changes

---
+++ src/src/components/App/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+
+import styles from './styles.scss';
+import index from '../../index.css';
+
+class App extends Component {
+	constructor(props) {
+    super(props);
+
+    this.state = {
+      
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Data Fusion Vis</div>
+      </div>
+    );
+  }
+}
+
+export default App;
